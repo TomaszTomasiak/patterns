ee9ee3605b748ce6fcd6b9a891c382c786733464
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-01-26 19:57:53 +0100

    prepared to implement facade pattern

--- src/main/java/com/patterns/facade/Order.java
+++ src/main/java/com/patterns/facade/Order.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class Order {
     private ProductService productService;
     private final List<Item> items = new ArrayList<>();
-    private final Long orderId;
-    private final Long userId;
+    private final long orderId;
+    private final long userId;
     private boolean isPaid = false;
     private boolean isVerified = false;
     private boolean isSubmitted = false;
--- src/main/java/com/patterns/facade/ShopService.java
+++ src/main/java/com/patterns/facade/ShopService.java
@@ -3,8 +3,8 @@ package com.patterns.facade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.*;
 
 @Service
 public class ShopService {
@@ -16,4 +16,114 @@ public class ShopService {
 
     @Autowired
     private ProductService productService;
+
+    public Long openOrder(long userId) {
+        if (authenticator.isAuthenticated(userId)) {
+            Long maxOrder = (long) orders.stream()
+                    .mapToInt(order -> order.getOrderId().intValue())
+                    .max().orElse(0);
+            orders.add(new Order(maxOrder + 1, userId, productService));
+            return maxOrder + 1;
+        } else {
+            return -1L;
+        }
+    }
+
+    public void addItem(long orderId, long productId, double qty) {
+        orders.stream()
+                .filter(order -> order.getOrderId().equals(orderId))
+                .forEach(order -> order.getItems().add(new Item(productId, qty)));
+    }
+
+    public boolean removeItem(long orderId, Long productId) throws Exception {
+
+//        Iterator<Order> orderIterator = orders.stream()
+//                .filter(order -> order.getOrderId().equals(orderId))
+//                .iterator();
+
+        Order theOrder = orders.stream()
+                .filter(order -> order.getOrderId().equals(orderId))
+                .findFirst().orElseThrow(Exception::new);
+
+        if (theOrder != null) {
+            for (Item tmpItem : theOrder.getItems()) {
+                if (tmpItem.getProductId().equals(productId)) {
+                    theOrder.getItems().remove(tmpItem);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public BigDecimal calculateValue(long orderId) throws Exception {
+
+        Order theOrder = orders.stream()
+                .filter(order -> order.getOrderId().equals(orderId))
+                .findFirst().orElseThrow(Exception::new);
+
+        if (theOrder != null) {
+            return theOrder.calculateValue();
+        } else {
+            return BigDecimal.ZERO;
+        }
+    }
+
+    public boolean doPayment(long orderId) throws Exception {
+        Order theOrder = orders.stream()
+                .filter(order -> order.getOrderId().equals(orderId))
+                .findFirst().orElseThrow(Exception::new);
+
+        if (theOrder != null) {
+            return theOrder.isPaid();
+        } else {
+            Random generator = new Random();
+            theOrder.setPaid(generator.nextBoolean());
+
+            return theOrder.isPaid();
+        }
+    }
+
+    public boolean verifyOrder(long orderId) throws Exception {
+        Order theOrder = orders.stream()
+                .filter(order -> order.getOrderId().equals(orderId))
+                .findFirst().orElseThrow(Exception::new);
+
+        if (theOrder != null) {
+            boolean result = theOrder.isPaid();
+            Random gen = new Random();
+            if(!theOrder.isVerified()) {
+                theOrder.setVerified(result && gen.nextBoolean());
+            }
+            return theOrder.isVerified();
+        }
+
+        return false;
+    }
+
+    public boolean submitOrder(long orderId) throws Exception {
+
+        Order theOrder = orders.stream()
+                .filter(order -> order.getOrderId().equals(orderId))
+                .findFirst().orElseThrow(Exception::new);
+
+        if (theOrder != null) {
+            if(theOrder.isVerified()) {
+                theOrder.setSubmitted((true));
+            }
+            return theOrder.isSubmitted();
+        }
+
+        return false;
+    }
+
+    public void cancelOrder(long orderId) throws Exception {
+        Order theOrder = orders.stream()
+                .filter(order -> order.getOrderId().equals(orderId))
+                .findFirst().orElseThrow(Exception::new);
+
+        if (theOrder != null) {
+           orders.remove(theOrder);
+        }
+    }
 }
