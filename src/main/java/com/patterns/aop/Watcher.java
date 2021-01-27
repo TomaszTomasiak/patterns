082f6fdd09b2938419c88c611a5a06ebcb14ce4f
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-01-27 21:01:19 +0100

    added aspects - FacadeWatcher.class - OrderFacade.processOrder()

--- src/main/java/com/patterns/aop/Watcher.java
+++ src/main/java/com/patterns/aop/Watcher.java
@@ -1,10 +1,14 @@
 package com.patterns.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
 
 @Component
 @Aspect
@@ -12,8 +16,23 @@ public class Watcher {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Watcher.class);
 
-    @Before("execution(* com.patterns.aop.Calculator.factorial(..))")
-    public void logEvent() {
-        LOGGER.info("Logging the event");
+    @Before(value = "execution(* com.patterns.aop.Calculator.factorial(..))" + "&& args(theNumber) && target(object)", argNames = "theNumber,object")
+    public void logEvent(BigDecimal theNumber, Object object) {
+        LOGGER.info("Class: " + object.getClass().getName() + ", Args: " + theNumber);
     }
+
+    @Around("execution(* com.patterns.aop.Calculator.factorial(..))")
+    public Object measureTime(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result;
+        try {
+            long begin = System.currentTimeMillis();
+            result = proceedingJoinPoint.proceed();
+            long end = System.currentTimeMillis();
+            LOGGER.info("Time consumed: " + (end - begin) + " [m/s]");
+        } catch (Throwable throwable) {
+            LOGGER.error(throwable.getMessage());
+            throw throwable;
+        }
+        return result;
+     }
 }
