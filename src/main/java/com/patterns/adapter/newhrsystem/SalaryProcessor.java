863c22e7931b987d577499ae72fd6a6fe18f6475
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-01-28 15:11:09 +0100

    preparation for adapter pattern implementation

---
+++ src/main/java/com/patterns/adapter/newhrsystem/SalaryProcessor.java
@@ -0,0 +1,8 @@
+package com.patterns.adapter.newhrsystem;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface SalaryProcessor {
+    BigDecimal calculateSalaries(List<Employee> employees);
+}
---
+++ src/main/java/com/patterns/adapter/oldhrsystem/SalaryCalculator.java
@@ -0,0 +1,5 @@
+package com.patterns.adapter.oldhrsystem;
+
+public interface SalaryCalculator {
+    double TotalSalary(String[][] workers, double[] salaries);
+}
